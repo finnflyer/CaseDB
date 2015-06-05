ffbf36fe29830f1a6077beb4d20adb4ff7e7ddf2
Chillhuang <Chillhuang@3224cf25-7a57-da4a-8b20-eca38d3cfec2>
2015-06-05 22:25:42 +0000

    git-svn-id: https://10.176.36.127/svn/CaseDB/trunk@5 3224cf25-7a57-da4a-8b20-eca38d3cfec2

--- src/com/lenovo/ittools/ctd/action/testplan/TestPlanHome.java
+++
@@ -1,9 +0,0 @@
-package com.lenovo.ittools.ctd.action.testplan;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class TestPlanHome extends ActionSupport {
-	public String exectue(){
-		return SUCCESS;
-	}
-}
